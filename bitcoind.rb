3b3c3d3b07efa1cef825428c8de41f31955978cc
Vincent Gariepy <caktux@gmail.com>
2013-12-15 19:57:19 -0500

    Update bitcoind to 0.8.6

--- bitcoind.rb
+++ bitcoind.rb
@@ -3,62 +3,130 @@ require 'formula'
 class Bitcoind < Formula
   homepage 'http://bitcoin.org/'
   head 'https://github.com/bitcoin/bitcoin.git'
-  url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.8.5'
-  version '0.8.5'
+  url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.8.6'
+  version '0.8.6'
 
-  devel do
+  head do
     url 'https://github.com/bitcoin/bitcoin.git', :branch => 'master'
     version 'master'
+
+    depends_on 'automake'
+    depends_on 'pkg-config'
+    depends_on 'protobuf'
   end
 
-  depends_on 'boost'
   depends_on 'berkeley-db4'
+  depends_on 'boost'
   depends_on 'miniupnpc' if build.include? 'with-upnp'
+  depends_on 'openssl'
 
   option 'with-upnp', 'Compile with UPnP support'
   option 'without-ipv6', 'Compile without IPv6 support'
 
   def patches
-    # fixes berkeley-db4 include and lib path
-    DATA
+    unless build.head?
+      # fix include and lib paths for berkeley-db4 and openssl
+      DATA
+    end
   end
 
   def install
-    cd "src" do
-      system "make", "-f", "makefile.osx",
-        "DEPSDIR=#{HOMEBREW_PREFIX}",
-        "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
-        "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
-      system "strip bitcoind"
-      bin.install "bitcoind"
+    raise 'Bitcoind currently requires --HEAD on Mavericks.' if MacOS.version == :mavericks and not build.head?
+
+    if build.head?
+      system "sh", "autogen.sh"
+      system "./configure", "--prefix=#{prefix}"
+      system "make"
+    else
+      cd "src" do
+        system "make", "-f", "makefile.osx",
+          "DEPSDIR=#{HOMEBREW_PREFIX}",
+          "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
+          "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
+      end
     end
+
+    system "strip src/bitcoind"
+    bin.install "src/bitcoind"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_prefix}/bin/bitcoind</string>
+          <string>-daemon</string>
+        </array>
+      </dict>
+    </plist>
+    EOS
   end
 
   def caveats; <<-EOS.undent
     You will need to setup your bitcoin.conf if you haven't already done so:
 
-    echo "rpcuser=user" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
-    echo "rpcpassword=password" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
+    echo -e "rpcuser=bitcoinrpc\\nrpcpassword=$(xxd -l 16 -p /dev/urandom)" > ~/Library/Application\\ Support/Bitcoin/bitcoin.conf
+    chmod 600 ~/Library/Application\\ Support/Bitcoin/bitcoin.conf
+
+    Use `bitcoind stop` to stop bitcoind if necessary! `brew services stop bitcoind` does not work!
     EOS
   end
 end
 __END__
 diff --git a/src/makefile.osx b/src/makefile.osx
-index 359739b..7b2f6e2 100644
+index 50279fd..6ab92b1 100644
 --- a/src/makefile.osx
 +++ b/src/makefile.osx
-@@ -13,11 +13,11 @@ INCLUDEPATHS= \
+@@ -7,18 +7,22 @@
+ # Originally by Laszlo Hanyecz (solar@heliacal.net)
+ 
+ CXX=llvm-g++
+ DEPSDIR=/opt/local
++DB4DIR=$(DEPSDIR)/opt/berkeley-db4
++OPENSSLDIR=$(DEPSDIR)/opt/openssl
+ 
+ INCLUDEPATHS= \
   -I"$(CURDIR)" \
-  -I"$(CURDIR)"/obj \
+- -I"$(CURDIR)"/obj \
++ -I"$(CURDIR)/obj" \
   -I"$(DEPSDIR)/include" \
 - -I"$(DEPSDIR)/include/db48"
-+ -I"$(DEPSDIR)/opt/berkeley-db4/include"
++ -I"$(DB4DIR)/include" \
++ -I"$(OPENSSLDIR)/include"
  
  LIBPATHS= \
   -L"$(DEPSDIR)/lib" \
 - -L"$(DEPSDIR)/lib/db48"
-+ -L"$(DEPSDIR)/opt/berkeley-db4/lib"
++ -L"$(DB4DIR)/lib" \
++ -L"$(OPENSSLDIR)/lib"
  
  USE_UPNP:=1
  USE_IPV6:=1
-
+ 
+@@ -31,13 +35,13 @@ ifdef STATIC
+ TESTLIBS += \
+  $(DEPSDIR)/lib/libboost_unit_test_framework-mt.a
+ LIBS += \
+- $(DEPSDIR)/lib/db48/libdb_cxx-4.8.a \
++ $(DB4DIR)/lib/libdb_cxx-4.8.a \
+  $(DEPSDIR)/lib/libboost_system-mt.a \
+  $(DEPSDIR)/lib/libboost_filesystem-mt.a \
+  $(DEPSDIR)/lib/libboost_program_options-mt.a \
+  $(DEPSDIR)/lib/libboost_thread-mt.a \
+  $(DEPSDIR)/lib/libboost_chrono-mt.a \
+- $(DEPSDIR)/lib/libssl.a \
+- $(DEPSDIR)/lib/libcrypto.a \
++ $(OPENSSLDIR)/lib/libssl.a \
++ $(OPENSSLDIR)/lib/libcrypto.a \
+  -lz
+ else
+ TESTLIBS += \
