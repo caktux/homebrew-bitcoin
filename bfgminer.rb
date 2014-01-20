148246a57a0cbf22a0b311b85ea1e893fcb0d514
Vincent Gariepy <caktux@gmail.com>
2014-01-20 04:27:49 -0500

    Update cgminer to 3.10.0, bfgminer to 3.9.0 and remove libmicrohttpd dependency

--- bfgminer.rb
+++ bfgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bfgminer < Formula
     homepage 'https://github.com/luke-jr/bfgminer'
     head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
-    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.8.1'
-    version '3.8.1'
+    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.9.0'
+    version '3.9.0'
     
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
@@ -14,7 +14,7 @@ class Bfgminer < Formula
     depends_on 'curl'
     depends_on 'jansson'
     depends_on 'libusb'
-    depends_on 'libmicrohttpd'
+    # depends_on 'libmicrohttpd'
     depends_on 'libevent'
 
     option 'with-cpumining', "Enable CPU mining"
